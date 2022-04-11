ebbe35614c8b19afe5196867216d0ec0fe58b0da
MaraD98 <84547390+MaraD98@users.noreply.github.com>
2022-04-11 20:44:23 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main (int argc, char *argv[]) {
-	cout<<"Hola Nahuel, este es el comienzo de mi tetris."<<endl;
+	cout<<"Hola amigos de youtube, este es el comienzo de mi tetris."<<endl;
 	return 0;
 }
 
